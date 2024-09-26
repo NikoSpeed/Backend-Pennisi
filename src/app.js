8c39c71020bfdc46ae19fcbdbab378eacbb6fda1
Nicolas <nicolaspennisi3gmail.com>
2024-09-26 12:52:53 -0300

    Entrega 1

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const productRouter = require("./routes/products.router.js")
+const cartRouter = require("./routes/carts.router.js")
+const app = express()
+const PUERTO = 8080
+
+app.use(express.json())
+
+app.use("/api/products", productRouter)
+app.use("/api/carts", cartRouter)
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el http://localhost:${PUERTO}`)
+})
